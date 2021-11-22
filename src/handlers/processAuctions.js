2ce85aed2860e04dd90fa313a9fe11771a1654de
wonkooklee <const.wonkook@gmail.com>
2021-11-23 00:57:58 +0900

    second commit

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,19 @@
+import createHttpError from "http-errors";
+import { getEndedAuctions } from "../lib/getEndedAuctions";
+import { closeAuction } from "../lib/closeAuction";
+
 async function processAuctions(event, context) {
-  console.log(`processing auctions!`);
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction)
+    );
+    await Promise.all(closePromises);
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.error(error);
+    throw new createHttpError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
